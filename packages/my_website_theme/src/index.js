0c3abe489b136278e7469f013d7c2b4c4f0e40ba
yintii <keleheart1@gmail.com>
2022-11-12 19:57:55 -0800

    Frontity set up

---
+++ packages/my_website_theme/src/index.js
@@ -0,0 +1,22 @@
+import Root from './components';
+
+const myWebsiteTheme = {
+  name: "my-website-theme",
+  roots: {
+    theme: Root,
+  },
+  state: {
+    theme: {
+      isUrlVisible: false,
+    },
+  },
+  actions: {
+    theme: {
+      toggleUrl: ({ state }) => {
+        state.theme.isUrlVisible = !state.theme.isUrlVisible;
+      }
+    },
+  },
+}
+
+export default myWebsiteTheme;
